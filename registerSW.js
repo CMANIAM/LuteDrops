fe2bffc1da4cae847c8a85388399b6b498e5f5ea
CMANIAM <CMANIAM@users.noreply.github.com>
2026-03-22 18:00:01 +0000

    deploy: 47004bf663b6c26cde1f31c0f8b892c786a26a2b

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lutedrops/sw.js', { scope: '/lutedrops/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/LuteDrops/sw.js', { scope: '/LuteDrops/' })})}
